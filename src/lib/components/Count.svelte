6900b660414d0feb80edf867af8768f0f7941e86
Jesús Enrique Rascón <blograso@gmail.com>
2023-09-24 16:35:01 +0200

    stroke almost working

--- src/lib/components/Count.svelte
+++ src/lib/components/Count.svelte
@@ -7,6 +7,15 @@
   let currentCount = habit.current_count;
   let targetCount = habit.target_count;
 
+  $: percentageValue = (habit.current_count / habit.target_count) * 100;
+  // $: percentageValue = 50;
+  console.log("percentage", habit.current_count / habit.target_count);
+
+  const radius = 33; // Radius of the circle
+  const circumference = 2 * Math.PI * radius;
+  $: dashArray = (percentageValue / 100) * circumference;
+  $: dashOffset = circumference - dashArray;
+
   const updateCurrentCount = async (id, current) => {
     try {
       const { data, error } = await supabaseClient
@@ -34,8 +43,44 @@
       e.preventDefault();
       updateCurrentCount(habit.id, currentCount);
     }}
-    class="btn btn-circle btn-lg btn-outline text-4xl flex flex-row rounded-full cursor-pointer fraction"
+    class="btn btn-circle btn-lg btn-ghost text-4xl flex flex-row rounded-full cursor-pointer fraction stroke-red-500"
   >
+    <!-- <svg width="100" height="100" class="absolute"> -->
+    <!--   <circle -->
+    <!--     cy="50" -->
+    <!--     cx="50" -->
+    <!--     r="30" -->
+    <!--     fill="none" -->
+    <!--     z="-10" -->
+    <!--     class="stroke-secondary completion" -->
+    <!--     stroke-dasharray={circumference} -->
+    <!--     stroke-dashoffset={dashOffset} -->
+    <!--     stroke-width="3" -->
+    <!--   /> -->
+    <!-- </svg> -->
+
+    <svg
+      class="absolute"
+      width="100"
+      height="100"
+      xmlns="http://www.w3.org/2000/svg"
+    >
+      <!-- Create a circle with no stroke -->
+      <!-- <circle cx="50" cy="50" r="40" fill="none" stroke="none" /> -->
+
+      <!-- Create a circle with a stroke that grows with the percentageValue -->
+      <circle
+        cx="50"
+        cy="50"
+        r={radius}
+        fill="none"
+        stroke-width="3"
+        class="stroke-secondary"
+        stroke-dasharray={circumference}
+        stroke-dashoffset={dashOffset}
+        transform="rotate(-90, 50, 50)"
+      />
+    </svg>
     <span class={ccountLength + tcountLength >= 4 ? "text-3xl" : ""}>
       <sup>{currentCount.toString()}</sup>⁄<sub>
         {habit.target_count.toString()}
