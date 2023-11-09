2a52e7c8f7ae9a79b2e745b0fa5ffd38b71079c0
Jesús Enrique Rascón <blograso@gmail.com>
2023-11-09 13:24:37 +0100

    upload error message

--- src/routes/+error.svelte
+++ src/routes/+error.svelte
@@ -1,8 +1,15 @@
+<script>
+  import { page } from "$app/stores";
+</script>
+
 <div class="flex flex-col justify-end">
   <div class="w-[80%] mx-auto my-20">
     <div class="text-6xl text-center mb-10">Woopsies...</div>
     <div class="text-3xl font-bold underline text-accent text-center">
       <a href="/app">Go back?</a>
+    </div>
+    <div class="">
+      {$page.error.message}
     </div>
   </div>
 </div>
